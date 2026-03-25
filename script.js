7940d5afcc761f0804109538e9e8c0e37884d1c7
rakshit kamboj <rakshitkamboj2@gmail.com>
2026-03-25 10:00:51 +0530

    Updated

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const API_KEY = "c9e046dfa1555b36adcb885255dab08e";
 
 const weatherBox = document.getElementById("weather");
 const historyBox = document.getElementById("history");
-
 async function getWeather(city) {
     const res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
